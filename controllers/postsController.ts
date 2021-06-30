24ae2bc86762d6115d3f507fabb1cc0e0e1a8a8c
jpcutshall <jpcutshall@gmail.com>
2021-06-29 21:07:54 -0400

    env

--- controllers/postsController.ts
+++ controllers/postsController.ts
@@ -4,11 +4,12 @@ import PostModel from '../models/PostSchema'
 
 const posts = express.Router()
 
-posts.get('/', async (req, res) => {
+posts.get('/', async (req, res, next) => {
     let postsArray = await PostModel.find()
     if (err) {
-        res.send(err)
+        next(err)
     }
-    
+
+
     
 })
